42d178bf4da6ca1ac32a4eaedc149e1498785810
Glauber Vilane Ferreira Marques <58866272+glaubervilane@users.noreply.github.com>
2020-02-01 23:13:36 -0600

    Update PORT on Index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path')
 
 const convert = require('./lib/convert')
 
+const port = process.env.PORT || 3000
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static('public'));
